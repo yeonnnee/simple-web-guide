5d5d0c7beb1424b0f1716f38017137f39e4324e1
yeonnnee <yeonnnee@gmail.com>
2023-10-03 22:56:22 +0900

    스크립트 수정

--- scripts/webSectionScript.js
+++ scripts/webSectionScript.js
@@ -18,67 +18,55 @@ const checkScrollIsEnd = (lastSection) => {
 
 window.addEventListener("scroll", () => {
   const scrollPos = window.scrollY;
-  const webPos = document.getElementById("웹이란").offsetTop;
-  const internetPos = document.getElementById("인터넷이란").offsetTop;
-  const webWorkPos =
+  const section1Pos = document.getElementById("웹이란").offsetTop;
+  const section2Pos = document.getElementById("인터넷이란").offsetTop;
+  const section3Pos =
     document.getElementById("웹은-어떻게-동작하는가").offsetTop;
-  const browserPos = document.getElementById("웹브라우저").offsetTop;
-  const browserEngineSectionPos =
-    document.getElementById("브라우저-구성-요소").offsetTop;
-  const renderingSectionPos = document.getElementById(
+  const section4Pos = document.getElementById("웹브라우저").offsetTop;
+  const section5Pos = document.getElementById("브라우저-구성-요소").offsetTop;
+  const section6Pos = document.getElementById(
     "브라우저-렌더링-과정-렌더링-엔진-동작-과정"
   ).offsetTop;
-  const clientPos = document.getElementById("클라이언트").offsetTop;
-  const dnsPos = document.getElementById("dns").offsetTop;
-  const webServerPos = document.getElementById("웹서버").offsetTop;
-  const wasPos = document.getElementById("was").offsetTop;
-  const httpPos = document.getElementById("http").offsetTop;
+  const section7Pos = document.getElementById("클라이언트").offsetTop;
+  const section8Pos = document.getElementById("dns").offsetTop;
+  const section9Pos = document.getElementById("웹서버").offsetTop;
+  const section10Pos = document.getElementById("was").offsetTop;
+  const section11Pos = document.getElementById("http").offsetTop;
 
-  const web = document.getElementsByClassName("web")[0];
-  const internet = document.getElementsByClassName("internet")[0];
-  const webWork = document.getElementsByClassName("web-work")[0];
-  const browser = document.getElementsByClassName("browser")[0];
-  const browserEngineSection =
-    document.getElementsByClassName("compose-browser")[0];
-  const browserRenderingSection =
-    document.getElementsByClassName("browser-rendering")[0];
-  const client = document.getElementsByClassName("client")[0];
-  const dns = document.getElementsByClassName("dns")[0];
-  const webServer = document.getElementsByClassName("web-server")[0];
-  const was = document.getElementsByClassName("was")[0];
-  const httpSection = document.getElementsByClassName("http")[0];
+  const section1 = document.getElementsByClassName("internet")[0];
+  const section2 = document.getElementsByClassName("web")[0];
+  const section3 = document.getElementsByClassName("web-work")[0];
+  const section4 = document.getElementsByClassName("browser")[0];
+  const section5 = document.getElementsByClassName("compose-browser")[0];
+  const section6 = document.getElementsByClassName("browser-rendering")[0];
+  const section7 = document.getElementsByClassName("client")[0];
+  const section8 = document.getElementsByClassName("dns")[0];
+  const section9 = document.getElementsByClassName("web-server")[0];
+  const section10 = document.getElementsByClassName("was")[0];
+  const section11 = document.getElementsByClassName("http")[0];
 
-  if (internetPos - scrollPos < 50 && webPos - scrollPos > 50) {
-    markCurrentSection(internet);
-  } else if (webPos - scrollPos < 80 && webWorkPos - scrollPos > 50) {
-    markCurrentSection(web);
-  } else if (webWorkPos - scrollPos < 50 && browserPos - scrollPos > 50) {
-    markCurrentSection(webWork);
-  } else if (
-    browserPos - scrollPos < 50 &&
-    browserEngineSectionPos - scrollPos > 50
-  ) {
-    markCurrentSection(browser);
-  } else if (
-    browserEngineSectionPos - scrollPos < 50 &&
-    renderingSectionPos - scrollPos > 50
-  ) {
-    markCurrentSection(browserEngineSection);
-  } else if (
-    renderingSectionPos - scrollPos < 50 &&
-    clientPos - scrollPos > 50
-  ) {
-    markCurrentSection(browserRenderingSection);
-  } else if (clientPos - scrollPos < 50 && dnsPos - scrollPos > 50) {
-    markCurrentSection(client);
-  } else if (dnsPos - scrollPos < 50 && webServerPos - scrollPos > 50) {
-    markCurrentSection(dns);
-  } else if (webServerPos - scrollPos < 50 && wasPos - scrollPos > 50) {
-    markCurrentSection(webServer);
-  } else if (wasPos - scrollPos < 50 && httpPos - scrollPos > 50) {
-    markCurrentSection(was);
+  if (section2Pos - scrollPos < 50 && section1Pos - scrollPos > 50) {
+    markCurrentSection(section1);
+  } else if (section1Pos - scrollPos < 80 && section3Pos - scrollPos > 50) {
+    markCurrentSection(section2);
+  } else if (section3Pos - scrollPos < 50 && section4Pos - scrollPos > 50) {
+    markCurrentSection(section3);
+  } else if (section4Pos - scrollPos < 50 && section5Pos - scrollPos > 50) {
+    markCurrentSection(section4);
+  } else if (section5Pos - scrollPos < 50 && section6Pos - scrollPos > 50) {
+    markCurrentSection(section5);
+  } else if (section6Pos - scrollPos < 50 && section7Pos - scrollPos > 50) {
+    markCurrentSection(section6);
+  } else if (section7Pos - scrollPos < 50 && section8Pos - scrollPos > 50) {
+    markCurrentSection(section7);
+  } else if (section8Pos - scrollPos < 50 && section9Pos - scrollPos > 50) {
+    markCurrentSection(section8);
+  } else if (section9Pos - scrollPos < 50 && section10Pos - scrollPos > 50) {
+    markCurrentSection(section9);
+  } else if (section10Pos - scrollPos < 50 && section11Pos - scrollPos > 50) {
+    markCurrentSection(section10);
   } else {
-    markCurrentSection(internet);
+    markCurrentSection(section1);
   }
-  checkScrollIsEnd(httpSection);
+  checkScrollIsEnd(section11);
 });
